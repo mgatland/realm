df601f51eee07455a7e339cb9c4aed404136dac8
mgatland <mgatland@gmail.com>
2016-11-28 21:39:03 +1300

    websocket server

---
+++ server/RealmServer/RealmServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace RealmServer
+{
+    public class GameplayService : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Send("echo: " + e.Data);
+        }
+        protected override void OnError(ErrorEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var server = new WebSocketServer(3000);
+            server.AddWebSocketService<GameplayService>("/game");
+            server.Start();
+            Console.ReadKey(true);
+            server.Stop();
+        }
+    }
+}
