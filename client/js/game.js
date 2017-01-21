8e3ecd8a3e393a0bec24bd2ab923dfc44c7ee997
Matthew Gatland <mgatland@gmail.com>
2017-01-21 21:47:28 +1300

    enemy AI keeps its distance from player

--- client/js/game.js
+++ client/js/game.js
@@ -1,13 +1,26 @@
 var GAME = (function () {
 	var game = {};
+
+	var aiCode = {};
+	aiCode.ranged = 0;
+
 	var p;
 	var e;
+
+	var util = {};
+	util.distance = function(a, b) {
+		return Math.sqrt((a.x - b.x) * (a.x - b.x)
+			+ (a.y - b.y) * (a.y - b.y));
+	}
+	util.dirFromTo = function(a, b) {
+		return Math.atan2(b.y - a.y, b.x - a.x);
+	}
 
 	game.init = function() {
 		game._player = p = {x: 0, y: 0, dir: 0, model:meshCode.ranger};
 		game._enemies = e = [];
 		for (var i = 0; i < 20; i++) {
-			var enemy = {x:0, y:0, dir:0, model:meshCode.mage};
+			var enemy = {x:0, y:0, dir:0, model:meshCode.mage, ai:aiCode.ranged};
 			enemy.x = Math.random() * -30 + 15;
 			enemy.y = Math.random() * -36 + 18;
 			enemy.dir = Math.random() * Math.PI * 2;
@@ -22,19 +35,41 @@ var GAME = (function () {
 		if (inputs.right) game._player.x += 0.2;
 
 		e.forEach(function (enemy) {
-			if (enemy.x > p.x) {
-				enemy.x -= 0.1;
-			}
-			if (enemy.x < p.x) {
-				enemy.x += 0.1;
-			}
-			if (enemy.y > p.y) {
-				enemy.y -= 0.1;
-			}
-			if (enemy.y < p.y) {
-				enemy.y += 0.1;
-			}
+			ai[enemy.ai](enemy);
 		});
+	}
+
+	var ai = [];
+	ai[aiCode.ranged] = function(enemy) {
+		var dist = util.distance(enemy, p);
+		if (dist > 13) {
+			var dir = util.dirFromTo(enemy, p);
+			var spd = 0.1;
+			var dX = Math.cos(dir) * spd;
+			var dY = Math.sin(dir) * spd;
+			enemy.x += dX;
+			enemy.y += dY;
+			enemy.timer = 0;
+		} else if (dist < 4) {
+			var dir = util.dirFromTo(p, enemy);
+			var spd = 0.1;
+			var dX = Math.cos(dir) * spd;
+			var dY = Math.sin(dir) * spd;
+			enemy.x += dX;
+			enemy.y += dY;
+			enemy.timer = 0;
+		} else {
+			if (enemy.timer == 0 || enemy.timer == undefined) {
+				enemy.timer = 20;
+				enemy.moveDir = Math.random() * Math.PI * 2;
+			}
+			enemy.timer--;
+			var spd = 0.1;
+			var dX = Math.cos(enemy.moveDir) * spd;
+			var dY = Math.sin(enemy.moveDir) * spd;
+			enemy.x += dX;
+			enemy.y += dY;
+		}
 	}
 
 	return game;
